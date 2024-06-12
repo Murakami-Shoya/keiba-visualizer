d46d43efc38c2a51bc9dd6258bf51ca0bcb8f4e7
Murakami-Shoya <66497863+Murakami-Shoya@users.noreply.github.com>
2024-06-12 21:58:54 +0900

    [update] テンプレートマッチング
    周辺の画素のみをマッチングする

--- frame.py
+++ frame.py
@@ -18,12 +18,19 @@ class Frame:
         self.gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # 探索範囲を指定
-        height, width, _ = image.shape
-        # 
+        self.height, self.width, _ = image.shape
+        self.left = int(self.width*0.18)
+        self.top = int(self.height*0.75)
+        self.right = int(self.width*0.82)
+        self.masked_gray = self.gray[self.top:self.height, self.left:self.right]
+        # self.masked_binary = cv2.threshold(self.masked_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]    # 2値化
         # ノイズを除去するために画像をぼかす
         # blurred = cv2.GaussianBlur(gray, (3, 3), 0) # 引数：画像、カーネルサイズ、標準偏差
+        
+        # templateの大きさ
+        self.template_shape = None
 
-
+    # 円検出
     def detect_circles(self):
         height, width, _ = self.image.shape
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
@@ -74,37 +81,95 @@ class Frame:
     #         self.df = self.df.append({'x': x, 'y': y, 'horse_num': horse_num}, ignore_index=True)
     #     return self.df
     
-    def draw_circles(self, circles):
-        # 検出された円を描画
-        if circles is not None:
-            # circles = np.uint16(np.around(circles))
-            for i in circles:
-                cv2.circle(self.image, (i[0], i[1]), i[2], (0, 255, 0), 2)
-                cv2.circle(self.image, (i[0], i[1]), 2, (0, 0, 255), 3) # 中心点を描画
-        # RGB画像を返す
-        return cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+    # def draw_circles(self, circles):
+    #     # 検出された円を描画
+    #     if circles is not None:
+    #         # circles = np.uint16(np.around(circles))
+    #         for i in circles:
+    #             cv2.circle(self.image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    #             cv2.circle(self.image, (i[0], i[1]), 2, (0, 0, 255), 3) # 中心点を描画
+    #     # RGB画像を返す
+    #     return cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
     
-    def pattern_matching(self, template_path):
+    def template_matching(self, template_path, mask=None):
         # 画像を読み込む
-        template = cv2.imread(template_path, 0)  # テンプレートはグレースケールで読み込む
-
-        # roi = self.gray[int(height*0.75):height, int(width*0.18):int(width*0.82)]
-
+        # template = cv2.imread(template_path, 0)  # テンプレートはグレースケールで読み込む
+        # templateは2値化して読み込む
+        template = cv2.imread(template_path, 0)
+        template = cv2.imread('template/i8.png', cv2.IMREAD_UNCHANGED)
+        _, alpha = cv2.threshold(template[:, :, 3], 0, 255, cv2.THRESH_BINARY)
+        # 透過部分を白にする
+        template[:, :, 0] = cv2.bitwise_and(template[:, :, 0], alpha)
+        template[:, :, 1] = cv2.bitwise_and(template[:, :, 1], alpha)
+        template[:, :, 2] = cv2.bitwise_and(template[:, :, 2], alpha)
+        # 透過部分を削除&グレースケールに変換
+        template = cv2.cvtColor(template, cv2.COLOR_BGRA2GRAY)
+        # _, template = cv2.threshold(template, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)    # 2値化
+        self.template_shape = template.shape
         # テンプレートマッチングを実行
-        res = cv2.matchTemplate(roi, template, cv2.TM_CCOEFF_NORMED)
+        if mask is not None:    # 前の検出部分の周辺のみを探索
+            left, top = int(mask[0]), int(mask[1])
+            near_pixel = 50
+            near_mask = (top-near_pixel, top+self.template_shape[0]+near_pixel, left-near_pixel, left+self.template_shape[1]+near_pixel)
+            # 画像サイズより大きくなる場合は，画像サイズに合わせる
+            near_mask = (max(near_mask[0], 0), min(near_mask[1], self.gray.shape[1]), max(near_mask[2], 0), min(near_mask[3], self.gray.shape[1]))
+            near_masked_gray = self.gray[near_mask[0]:near_mask[1], near_mask[2]:near_mask[3]]
+            res = cv2.matchTemplate(near_masked_gray, template, cv2.TM_CCOEFF_NORMED)
+        else:
+            res = cv2.matchTemplate(self.masked_gray, template, cv2.TM_CCOEFF_NORMED)
 
         # 最も一致する位置を取得
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
         top_left = max_loc
-        bottom_right = (top_left[0] + template.shape[1], top_left[1] + template.shape[0])
+        # bottom_right = (top_left[0] + template.shape[1], top_left[1] + template.shape[0])
+        print(max_val)
+        if max_val < 0.5:
+            # print("テンプレートが見つかりませんでした。")
+            # return 1, 1, 0, 0
+            return mask
+        else:
+            # center = top_left[0] + template.shape[1]//2, top_left[1] + template.shape[0]//2
+            # return self.left+top_left[0], self.top+top_left[1], self.left+bottom_right[0], self.top+bottom_right[1] # 左上と右下の座標を返す
+            if mask is not None:
+                return top_left[0]+left-near_pixel, top_left[1]+top-near_pixel, self.template_shape[1], self.template_shape[0]
+            else:
+                return self.left+top_left[0], self.top+top_left[1], self.template_shape[1], self.template_shape[0] # 左上と幅，高さの座標を返す
+            # return top_left[0], top_left[1], top_left[0]+template.shape[1], top_left[1]+template.shape[0] # template.shape[0]が高さ，template.shape[1]が幅
+            # return top_left[0], top_left[1], template.shape[1], template.shape[0] # template.shape[0]が高さ，template.shape[1]が幅
+        # # 元の画像に結果を反映
+        # cv2.rectangle(frame, (self.left+top_left[0], self.top+top_left[1]), (self.left+bottom_right[0], self.top+bottom_right[1]), (0, 255, 0), 2)
+
+    def draw_circles(self, center, r=35//2):
+        # 検出された円を描画
+        fixed_center = (center[0]+self.left, center[1]+self.top)
+
+        cv2.circle(self.image, fixed_center, r, (0, 255, 0), 2)
+        cv2.circle(self.image, fixed_center, 2, (0, 0, 255), 3) # 中心点を描画
+        # RGB画像を返す
+        # return cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+
+    def draw_rectangle(self, bbox):
+        # cv2.rectangle(self.image, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 255, 0), 2)
+        # p1 = (int(bbox[0]), int(bbox[1]))
+        # p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        int_bbox = (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))
+        cv2.rectangle(self.image, int_bbox, (0, 255, 0), 2)
 
 
 if __name__ == "__main__":
     image = cv2.imread("frame_500.jpg")
     frame = Frame(image)
-    circles = frame.detect_circles()
-    image_with_circles = frame.draw_circles(circles)
-    plt.imshow(image_with_circles)
-    plt.show()
+    # circles = frame.detect_circles()
+    # image_with_circles = frame.draw_circles(circles)
+
+    horese8_bbox = frame.template_matching("./template/i8.png")
+    frame.draw_rectangle(horese8_bbox)
+
+    cv2.imshow('Frame', frame.image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    # plt.imshow(image_with_circles)
+    # plt.show()
     # df = visualizer.detect_horses(circles)
     # print(df)
--- main.py
+++ main.py
@@ -1,27 +1,71 @@
 import cv2
 
 from video import Video
+from frame import Frame
+from horse import Horse
+from visualizer import Visualizer
 
 youtube_path = "2024年 京王杯スプリングカップ（GⅡ）  ウインマーベル  JRA公式.mp4"
-skip_frame = 400
+skip_frame = 600
+template_size = (35, 35)
 
 if __name__ == "__main__":
+    youtube_path = "2024年 京王杯スプリングカップ（GⅡ）  ウインマーベル  JRA公式.mp4"
     keio_spring_cup = Video(youtube_path)
-    # keio_spring_cup.play_with_plt()
+    v = Visualizer()
 
-    # # skip_frameフレーム目までスキップ
-    # for i in range(frame_count):
-    #     ret, frame = cap.read()
+    horse8 = Horse(8, "red")
+
+    for _ in range(skip_frame):
+        ret, frame = keio_spring_cup.cap.read()
+    
+    f = Frame(frame)
+    horese8_left = f.template_matching("./template/i8.png")
+    horse8_bbox = (horese8_left[0], horese8_left[1], horese8_left[0]+template_size[1], horese8_left[1]+template_size[0])
+
+    # v.tracker.add(cv2.legacy.TrackerKCF_create(), frame, horse8_bbox)
+    # v.tracker.add(cv2.legacy.TrackerCSRT_create(), frame, horse8_bbox)
+
+    for num_frame in range(keio_spring_cup.frame_count):
+        # self.cap.set(cv2.CAP_PROP_POS_FRAMES, 500)
+        ret, frame = keio_spring_cup.cap.read()
+
+        # フレームが正しく読み込めなかった場合、ループを抜ける
+        if not ret:
+            print("フレームを読み込めませんでした。動画の終了またはエラーです。")
+            break
         
-    #     if i <= skip_frame - 1:
-    #         continue
-    #     else:
-    #         # 1フレームごと馬の座標を検出
-    #             # フレームを表示
-    #         cv2.imshow('Frame', frame)
+        
+        f = Frame(frame)
+        horese8_left = f.template_matching("./template/i8.png", horse8_bbox)
+        horse8_bbox = (horese8_left[0], horese8_left[1], template_size[1], template_size[0])
+        # circles = f.draw_circles(f.detect_circles())     
+        # パターンマッチング
+        # horese8_bbox = f.pattern_matching("./template/i8.png")
+        # トラッキング
+        # ok, bboxes = v.tracker.update(frame)
+        # template_sizeを使ってbboxesを修正
+        # fixed_bboxes = [(bbox[0], bbox[1], template_size[1], template_size[0]) for bbox in bboxes]
 
-    #         # 'q'キーが押されたらループから抜ける
-    #         if cv2.waitKey(1) & 0xFF == ord('q'):
-    #             break
+        # race.dfの一番下の行に8番の馬の座標を追加
+        # v.ax.clear()    # グラフをクリア
+        # v.add_location(horse8, horese8_bbox, num_frame)
+        # v.visualize(horse8.number)
 
+        
+        # v.show()
+
+        
+
+
+        f.draw_rectangle(horse8_bbox)
+        
+        # RGB画像として表示
+        cv2.imshow('Frame', frame)
+
+
+        # 'q'キーが押されたらループから抜ける
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            print(v.df)
+            break
 
